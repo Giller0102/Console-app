51fdad32d98d9d8afc5aba04bfba60c75da542d5
Lera Gilevskaya <hilevskayavaleriya@gmail.com>
2023-02-27 20:58:42 +0300

    a beginning of console app

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strick';
+
+const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+
+

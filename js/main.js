18699504ababffb2a4e2ec00600a834931ce358b
Lera Gilevskaya <hilevskayavaleriya@gmail.com>
2023-03-07 20:47:56 +0300

    I finished second practical part of learninng JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strick';
+'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
 
@@ -10,15 +10,49 @@ let personalMovieDB = {
     privat: false
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i <=2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+    
+
+    if (a != '' && b != '' && a != null && b != null && a.length > 50 && b.length > 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');    
+    }
+}
+
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Error');
+}
+
 
 console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
